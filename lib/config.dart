478b205ff0bb0e2094099f04ca2f6e50f0b11d64
Paizqbal <118704144+Paizqbal@users.noreply.github.com>
2023-01-31 21:27:51 +0800

    Final Project HomstayRaya

---
+++ lib/config.dart
@@ -0,0 +1,5 @@
+class CONFIG {
+  //static const String server = "https://192.168.0.132/homestayraya1";
+  static const String server = "http://10.19.95.230/homestayraya";
+  //10.31.245.8
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:homestayraya/view/MyScreen/splashscreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Homestay Raya',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
